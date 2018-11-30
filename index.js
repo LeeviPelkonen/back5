f4b9cf80e6ea956cdf36c3f81ac754ce854c091c
LeeviPelkonen <32296789+LeeviPelkonen@users.noreply.github.com>
2018-11-30 11:10:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,92 +1,68 @@
 'use strict';
+
 require('dotenv').config();
 const express = require('express');
-const fs = require('fs');
-const https = require('https');
-const http = require('http');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
-const app = express();
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
-
 const db = require('./modules/database');
+const resize = require('./modules/resize');
+const exif = require('./modules/exif');
+
+const multer = require('multer');
+const upload = multer({dest: 'public/uploads/'});
+
+const app = express();
+
 const connection = db.connect();
 
-passport.serializeUser((user, done) => {
-  console.log('serialize: ' + user);
-  done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
-
-app.use(session({
-  secret: 'keyboard LOL cat',
-  resave: true,
-  saveUninitialized: true,
-  cookie: {secure: true},
-}));
-
-passport.use(new LocalStrategy(
-    (username, password, done) => {
-      console.log('Here we go: ' + username);
-      let res = null;
-
-      const doLogin = (username, password) => {
-        return new Promise((resolve, reject) => {
-          db.login([username, password], connection, (result) => {
-            console.log('result', result.length);
-            resolve(result);
-          });
-        });
-      };
-
-      return doLogin(username, password).then((res) => {
-        if (res.length < 1) {
-          console.log('undone');
-          return done(null, false);
-        } else {
-          console.log('done');
-          return done(null, {username: username});
-        }
-      });
-
-    },
-));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.post('/login',
-    passport.authenticate('local',
-        {successRedirect: '/node/', failureRedirect: '/node/login.html'}));
-
-app.set('trust proxy');
-const sslkey = fs.readFileSync('/etc/pki/tls/private/ca.key');
-const sslcert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
-const options = {
-  key: sslkey,
-  cert: sslcert,
+const cb = (result, res) => {
+  console.log(result);
+  res.send(result);
 };
 
-app.get('/', (req, res) => {
-  if (req.secure) {
-    console.log('req.user', req.user);
-    if (req.user !== undefined) res.send('Hello ' + req.user.username);
-    else res.redirect(301, './login.html');
-  }
-  else res.send('hello not secure?');
+app.use(express.static('public'));
+
+// respond to post and save file
+app.post('/upload', upload.single('mediafile'), (req, res, next) => {
+  next();
 });
 
-//app.listen(8000);
-http.createServer((req, res) => {
-  const redir = 'https://' + req.headers.host + '/node' + req.url;
-  console.log('redir', redir);
-  res.writeHead(301, {'Location': redir});
-  res.end();
-}).listen(8000);
-https.createServer(options, app).listen(3000);
+// create thumbnail
+app.use('/upload', (req, res, next) => {
+  resize.doResize(req.file.path, 300, './public/thumbnails/' + req.file.filename + '_thumb', next);
+  next();
+});
+
+// create medium image
+app.use('/upload', (req, res, next) => {
+  resize.doResize(req.file.path, 640, './public/medium/' + req.file.filename + '_medium', next);
+  next();
+});
+
+// get coordinates
+app.use('/upload', (req, res, next) => {
+  exif.getCoordinates(req.file.path).then(coords => {
+    req.coordinates = coords;
+    console.log('the coords are: '  +coords)
+    next();
+  });
+});
+
+// insert to database
+app.use('/upload', (req, res, next) => {
+  const data = [
+    req.body.category,
+    req.body.title,
+    req.body.details,
+    req.file.filename + '_thumb',
+    req.file.filename + '_medium',
+    req.file.filename,
+    req.coordinates,
+  ];
+  db.insert(data, connection, next);
+});
+
+// get updated data form database and send to client
+app.use('/upload', (req, res) => {
+  db.select(connection, cb, res);
+});
+
+app.listen(3000);
